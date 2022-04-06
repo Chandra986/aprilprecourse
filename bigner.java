fec2a84fe345f2ba2df420ac86dd80bee7b022c4
Chandra Prakash <schandraprakash51@gmail.com>
2022-04-06 23:00:09 +0530

    Initial commit

---
+++ bigner.java
@@ -0,0 +1,21 @@
+public class bigner {
+    public staic void main(String [] args){
+
+        int a=10; int b=20; int c=30;
+        if(a>b){
+            if(a>c)
+                System.out.print(a);
+                else
+                    System.out.print(c);
+        }
+
+
+        else (){
+            if(b>c)
+                System.out.print(b);
+            else
+                system.out.print(c);
+
+        }
+    }
+}
